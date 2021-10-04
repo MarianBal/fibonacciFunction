ffa63f034bd80122a7554cc1693a15c7ec88f298
Marian <mariana_balsamo@hotmail.com>
2021-10-04 12:50:31 -0300

    create function

--- fibonacci.js
+++ fibonacci.js
@@ -1,3 +1,18 @@
-const fibonacci = number => number;
+const fibonacci = number =>{
 
-console.log(fibonacci(1));
+  let init = 0;
+  let add = 1;
+  let result;
+  
+  for (let i=0; number>= i; i++){
+    if(i ===0) result= init && console.log('0');
+    if(i ===1) result = add && console.log('1');
+    if(i > 1){
+      result = init + add;
+      init = add;
+      add = result;}  
+    }
+  return result
+}
+
+//console.log(fibonacci(7));
